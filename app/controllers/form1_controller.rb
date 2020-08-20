514ec22183acf5ca23206c64c4f9dae0fd26796d
quan18070712 <quan.nguyen_bk2017@hcmut.edu.vn>
2020-08-20 11:27:07 +0700

    edit form and create filter table

--- app/controllers/form1_controller.rb
+++ app/controllers/form1_controller.rb
@@ -3,25 +3,30 @@ class Form1Controller < ApplicationController
     @form1 = Form1.where(user_id: current_user.id)
     respond_to do |format|
       format.html
-      format.csv { send_data @form1.as_csv}
+      format.csv { send_data @form1.as_csv }
     end
   end
+
   def new
     @form1 = Form1.new
   end
+
   def show
     @form1 = Form1.where(user_id: current_user.id)
   end
+
   def create
-    @form1 =  Form1.new(form1_params.merge(user_id: current_user.id))
+    @form1 = Form1.new(form1_params.merge(user_id: current_user.id))
     if @form1.save
       redirect_to current_user
     else
-      render 'new'
+      render "new"
     end
   end
+
   private
-    def form1_params
-      params.require(:form1).permit(:name, :time_work, :job, :date, :customer, :income, :work_des)
-    end
+
+  def form1_params
+    params.require(:form1).permit(:name, :time_work, :job, :date, :customer, :income, :work_des)
+  end
 end
