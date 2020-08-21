a00396cf9a7a647051b2f475241a52a90f2f2bf8
quan18070712 <quan.nguyen_bk2017@hcmut.edu.vn>
2020-08-21 13:02:15 +0700

    update UI second

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,44 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :destroy]
-  #before_action :correct_user
-  before_action :admin_user,     only: :destroy
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def show
     @user = User.find(params[:id])
   end
+
   def index
-    @user = User.all 
+    @user = User.all
   end
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
       redirect_to @user
     else
-      render 'new'
+      render "new"
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
@@ -27,24 +46,27 @@ class UsersController < ApplicationController
   end
 
   private
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
-    # Confirms the correct user.
-    #def correct_user
-      #@user = User.find(params[:id])
-      #redirect_to(root_url) unless current_user?(@user)
-    #end
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+  #Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
